430b462a47b317fe054f26aac8fe230d727c9718
PedroCella <110991040+PedroCella@users.noreply.github.com>
2022-09-07 20:33:07 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,26 @@
+basic.forever(function () {
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        # . . . #
+        . # # # .
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . # . .
+        . # . # .
+        . . # . .
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        # # # # #
+        . . . . .
+        `)
+    basic.pause(1000)
+})
